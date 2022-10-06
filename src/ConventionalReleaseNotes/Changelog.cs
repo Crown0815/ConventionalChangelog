30f001b4d91ec4bb6f4f31a47c257119ea654a31
Felix Kröner <kroener@dynamic-biosensors.com>
2022-10-06 21:14:56 +0200

    style: Add missing comma before linebreak
    #SDPI-11

--- src/ConventionalReleaseNotes/Changelog.cs
+++ src/ConventionalReleaseNotes/Changelog.cs
@@ -8,7 +8,7 @@ public class Changelog
     {
         new("feat: ", "Features"),
         new("fix: ", "Bug Fixes"),
-        new("perf: ", "Performance Improvements")
+        new("perf: ", "Performance Improvements"),
     };
 
     private const string BulletPoint = "- ";
