d2ac70e8fa39f1b91448f7c65eaf09fc19436a40
Felix Kröner <kroener@dynamic-biosensors.com>
2022-11-30 13:30:30 +0100

    refactor: Upgrade changelog specs from class to namespace
    #SDPI-11

--- ConventionalReleaseNotes.Unit.Tests/Changelog_specs/A_changelog_from.cs
+++ ConventionalReleaseNotes.Unit.Tests/Changelog_specs/A_changelog_from.cs
@@ -2,16 +2,16 @@ using System;
 using FluentAssertions;
 using Xunit;
 
-namespace ConventionalReleaseNotes.Unit.Tests;
+namespace ConventionalReleaseNotes.Unit.Tests.Changelog_specs;
 
-public partial class Changelog_specs
+public class A_changelog_from
 {
     [Theory]
     [InlineData(null)]
     [InlineData(new object[] { new string[] { null! } })]
     [InlineData(new object[] { new string[] { null!, null! } })]
     [InlineData(new object[] { new[] { "", null!, "" } })]
-    public void A_changelog_from_null_throws_null_exception(string[] @null)
+    public void null_throws_null_exception(string[] @null)
     {
         Action fromNull = () => Changelog.From(@null);
         fromNull.Should().Throw<Exception>();
@@ -21,7 +21,7 @@ public partial class Changelog_specs
     [InlineData]
     [InlineData("")]
     [InlineData("", "")]
-    public void A_changelog_from_empty_changes_is_empty(params string[] noChanges)
+    public void empty_changes_is_empty(params string[] noChanges)
     {
         var changelog = Changelog.From(noChanges);
         changelog.Should().Be(Model.Changelog.Empty);
@@ -30,7 +30,7 @@ public partial class Changelog_specs
     [Theory]
     [InlineData("some message")]
     [InlineData("1234: abc")]
-    public void A_changelog_from_non_conventional_commits_is_empty(string nonConventionalCommits)
+    public void non_conventional_commits_is_empty(string nonConventionalCommits)
     {
         var changelog = Changelog.From(nonConventionalCommits);
         changelog.Should().Be(Model.Changelog.Empty);
@@ -44,7 +44,7 @@ public partial class Changelog_specs
     [InlineData("style")]
     [InlineData("refactor")]
     [InlineData("test")]
-    public void A_changelog_from_changelog_irrelevant_conventional_commits_contains_general_code_improvements_message(string indicator)
+    public void changelog_irrelevant_conventional_commits_contains_general_code_improvements_message(string indicator)
     {
         var conventionalCommit1 = Model.ConventionalCommitMessage(indicator, "unused");
         var conventionalCommit2 = Model.ConventionalCommitMessage(indicator, "unused");
---
+++ ConventionalReleaseNotes.Unit.Tests/Changelog_specs/A_changelog_from_relevant_conventional_commits.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Xunit;
+using static ConventionalReleaseNotes.Unit.Tests.CommitType;
+
+namespace ConventionalReleaseNotes.Unit.Tests.Changelog_specs;
+
+public class A_changelog_from_relevant_conventional_commits
+{
+    public static readonly object[][] ChangelogRelevantCommitTypes =
+    {
+        new object[] { Feature },
+        new object[] { Bugfix },
+        new object[] { PerformanceImprovement },
+    };
+
+    private readonly Model.Changelog _changelog = Model.Changelog.Empty;
+
+    [Theory]
+    [MemberData(nameof(ChangelogRelevantCommitTypes))]
+    public void is_the_changelog_header_plus_a_group_containing_the_descriptions(ConventionalCommitType type)
+    {
+        var message1 = Model.ConventionalCommitMessage(type.Indicator, Model.Message(1));
+        var message2 = Model.ConventionalCommitMessage(type.Indicator, Model.Message(2));
+
+        var changelog = Changelog.From(message1, message2);
+
+        changelog.Should().Be(_changelog
+            .WithGroup(type.Header)
+                .WithBullet(Model.Message(1))
+                .WithBullet(Model.Message(2)));
+    }
+
+    [Fact]
+    public void and_irrelevant_commits_contains_all_relevant_entries()
+    {
+        var message1 = Model.ConventionalCommitMessage(Feature.Indicator, Model.Message(1));
+        var message2 = Model.ConventionalCommitMessage("chore", Model.Message(2));
+
+        var changelog = Changelog.From(message1, message2);
+
+        changelog.Should().Be(_changelog
+            .WithGroup(Feature.Header)
+                .WithBullet(Model.Message(1)));
+    }
+
+    [Fact]
+    public void in_random_order_is_for_each_type_the_changelog_header_plus_a_group_containing_the_descriptions()
+    {
+        var messages = new[]
+        {
+            Model.ConventionalCommitMessage(Feature.Indicator, Model.Message(1)),
+            Model.ConventionalCommitMessage(Bugfix.Indicator, Model.Message(2)),
+            Model.ConventionalCommitMessage(PerformanceImprovement.Indicator, Model.Message(3)),
+            Model.ConventionalCommitMessage(Feature.Indicator, Model.Message(4)),
+            Model.ConventionalCommitMessage(PerformanceImprovement.Indicator, Model.Message(5)),
+            Model.ConventionalCommitMessage(Bugfix.Indicator, Model.Message(6)),
+        };
+
+        var changelog = Changelog.From(messages);
+
+        changelog.Should().Be(_changelog
+            .WithGroup(Feature.Header)
+                .WithBullet(Model.Message(1))
+                .WithBullet(Model.Message(4))
+            .WithGroup(Bugfix.Header)
+                .WithBullet(Model.Message(2))
+                .WithBullet(Model.Message(6))
+            .WithGroup(PerformanceImprovement.Header)
+                .WithBullet(Model.Message(3))
+                .WithBullet(Model.Message(5)));
+    }
+}
