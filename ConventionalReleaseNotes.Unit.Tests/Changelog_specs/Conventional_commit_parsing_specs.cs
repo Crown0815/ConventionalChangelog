831aa9a3fc5ae2c53fe9a1f0bd5c9bf753fd54d9
Felix Kröner <kroener@dynamic-biosensors.com>
2022-11-30 22:04:05 +0100

    test: Implement parser to transform raw commit message into structured conventional commit message object
    #SDPI-11

---
+++ ConventionalReleaseNotes.Unit.Tests/Changelog_specs/Conventional_commit_parsing_specs.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace ConventionalReleaseNotes.Unit.Tests.Changelog_specs;
+
+public class Conventional_commit_parsing_specs
+{
+    // Conventional commit specification:
+    // https://www.conventionalcommits.org/en/v1.0.0/#specification
+
+    private static class ConventionalCommit
+    {
+        public const string Type = "type";
+        public const string Description = "description";
+
+        public const string Body = """
+                                   Body with
+                                   newlines 	 tabs
+
+                                      and some spaces
+
+
+                                   and multiple blank lines
+                                   """;
+
+        public const string FooterToken = "token";
+        public const string FooterValue = "value";
+        public const string Footer = $"{FooterToken}: {FooterValue}";
+
+        public const string Message = $"""
+            {Type}: {Description}
+
+            {Body}
+
+            {Footer}
+            """ ;
+    }
+
+    private readonly ConventionalCommitMessage _parsed;
+
+    public Conventional_commit_parsing_specs()
+    {
+        _parsed = ConventionalCommitMessage.Parse(ConventionalCommit.Message);
+    }
+
+    [Fact]
+    public void The_parser_extracts_the_type_indicator_from_a_conventional_commit()
+    {
+        _parsed.Type.Should().Be(ConventionalCommit.Type);
+    }
+
+    [Fact]
+    public void The_parser_extracts_the_description_from_a_conventional_commit()
+    {
+        _parsed.Description.Should().Be(ConventionalCommit.Description);
+    }
+
+    [Fact]
+    public void The_parser_extracts_the_body_from_a_conventional_commit()
+    {
+        _parsed.Body.Should().Be(ConventionalCommit.Body);
+    }
+
+    [Fact]
+    public void The_parser_extracts_the_footer_from_a_conventional_commit()
+    {
+        _parsed.Footers.Should().BeEquivalentTo(new ConventionalCommitFooter[]
+        {
+            new(ConventionalCommit.FooterToken, ConventionalCommit.FooterValue),
+        });
+    }
+
+    private static readonly string[] Tokens =
+    {
+        "token",
+        "token-with-dash",
+        "BREAKING CHANGE",
+        "BREAKING-CHANGE",
+    };
+
+    private static readonly string[] Separators = { ": ", " #" };
+
+    private static readonly string[] Values =
+    {
+        "value",
+        "value with spaces",
+        "value	with	tabs",
+        """
+        value with
+        linebreak
+        """,
+        """
+        value with
+
+        blank line
+        """,
+    };
+
+    public static IEnumerable<object[]> GitTrailerConventionFooters()
+    {
+        foreach (var token in Tokens)
+        foreach (var separator in Separators)
+        foreach (var value in Values)
+            yield return new object[] { token + separator + value, token, value };
+    }
+
+    public static IEnumerable<object[]> YouTrackConventionFooters()
+    {
+        yield return new object[] { "#SWX-1234", "SWX-1234", "" };
+        yield return new object[] { "#SWX-1234 command", "SWX-1234", "command" };
+        yield return new object[] { "#SWX-1234 command1 command2", "SWX-1234", "command1 command2" };
+    }
+
+    [Theory]
+    [MemberData(nameof(GitTrailerConventionFooters))]
+    [MemberData(nameof(YouTrackConventionFooters))]
+    public void The_parser_extracts_the_specific(string footer, string expectedToken, string expectedValue)
+    {
+        var messageWithSpecificFooter = ConventionalCommit.Message.Replace(ConventionalCommit.Footer, footer);
+        var parsed = ConventionalCommitMessage.Parse(messageWithSpecificFooter);
+
+        parsed.Footers.Should().BeEquivalentTo(new ConventionalCommitFooter[]
+        {
+            new(expectedToken, expectedValue),
+        });
+    }
+
+    [Fact]
+    public void The_parser_extracts_multiple_footers()
+    {
+        const string footers = """
+                      token1: value1
+                      token-2: value 2
+
+                      token3 #value 3
+                      token-4 #value
+                      with extra line
+
+                      token-5: value
+
+                      with blank line
+                      """;
+
+        var messageWithSpecificFooter = ConventionalCommit.Message.Replace(ConventionalCommit.Footer, footers);
+        var parsed = ConventionalCommitMessage.Parse(messageWithSpecificFooter);
+
+        parsed.Footers.Should().BeEquivalentTo(new ConventionalCommitFooter[]
+        {
+            new("token1", "value1"),
+            new("token-2", "value 2"),
+            new("token3", "value 3"),
+            new("token-4", $"value{Environment.NewLine}with extra line"),
+            new("token-5", $"value{Environment.NewLine}{Environment.NewLine}with blank line"),
+        });
+    }
+}
---
+++ src/ConventionalReleaseNotes/ConventionalCommitMessage.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConventionalReleaseNotes;
+
+public record ConventionalCommitMessage(string Type, string Description, string Body, IReadOnlyCollection<ConventionalCommitFooter> Footers)
+{
+    public static ConventionalCommitMessage Parse(string rawMessage)
+    {
+        using var reader = new StringReader(rawMessage);
+        var header = reader.ReadLine()!;
+        var parts = header.Split(": ");
+        var type = parts.First();
+        var description = header.Replace(type+": ", "");
+
+        var (body, footers) = BodyFrom(reader);
+
+        return new ConventionalCommitMessage(type, description.Trim(), body, footers.ToList());
+    }
+
+    private static IEnumerable<string> LinesFrom(TextReader reader)
+    {
+        while (reader.ReadLine() is {} line)
+            yield return line;
+    }
+
+    private static IEnumerable<ConventionalCommitFooter> FootersFrom(IEnumerable<string> lines)
+    {
+        ConventionalCommitFooter? buffer = null;
+        foreach (var line in lines)
+        {
+            if (buffer is not null && IsFooter(line))
+            {
+                yield return buffer with {Value = buffer.Value.Trim()};
+                buffer = null;
+            }
+            if (buffer is null && string.IsNullOrWhiteSpace(line))
+                continue;
+            if (buffer is not null)
+                buffer = buffer with { Value = buffer.Value + Environment.NewLine + line};
+            else
+                buffer = FooterFrom(line);
+        }
+
+        if (buffer is not null)
+            yield return buffer with {Value = buffer.Value.Trim()};
+    }
+
+    private static (string, IEnumerable<ConventionalCommitFooter>) BodyFrom(TextReader reader)
+    {
+        var bodyParts = new List<string>();
+        var footers = Enumerable.Empty<ConventionalCommitFooter>();
+        while (reader.ReadLine() is { } line)
+        {
+            if (IsFooter(line))
+            {
+                footers = FootersFrom(LinesFrom(reader).Prepend(line));
+                break;
+            }
+            bodyParts.Add(line);
+        }
+
+        return (string.Join(Environment.NewLine, bodyParts).Trim(), footers);
+    }
+
+    private static bool IsFooter(string line)
+    {
+        if (Regex.IsMatch(line, @"^#\w+-\d+")) return true;
+        return Regex.IsMatch(line, @"^(?<token>[\w\-]+|BREAKING[ -]CHANGE)(?<separator>: | #)");
+    }
+
+    private static ConventionalCommitFooter FooterFrom(string line)
+    {
+        if (Regex.IsMatch(line, @"^#\w+-\d+"))
+        {
+            var x = line.Split(" ");
+            var token1 = x.First().Replace("#", "");
+            var value1 = line.Replace("#"+token1, "").Trim();
+            return new ConventionalCommitFooter(token1, value1);
+        }
+
+        var match = Regex.Match(line, @"^(?<token>[\w\-]+|BREAKING[ -]CHANGE)(?<separator>: | #)");
+
+        var token = match.Groups["token"].Value;
+        var value = line.Replace(match.Value, "");
+        return new ConventionalCommitFooter(token, value);
+    }
+
+}
+
+public record ConventionalCommitFooter(string Token, string Value);
