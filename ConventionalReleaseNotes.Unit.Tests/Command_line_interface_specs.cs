e6ef35b2a080651ff6e851a62ad812923a8d842c
Felix Kröner <kroener@dynamic-biosensors.com>
2022-12-13 21:22:14 +0100

    feat: Enable generation of changelog from git repository
    #SDPI-11

---
+++ ConventionalReleaseNotes.Unit.Tests/Command_line_interface_specs.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using System.Diagnostics;
+using FluentAssertions;
+using static System.Environment;
+using static ConventionalReleaseNotes.Unit.Tests.CommitTypeFor;
+
+namespace ConventionalReleaseNotes.Unit.Tests;
+
+public class Command_line_interface_specs : GitUsingTestsBase
+{
+    [Fact]
+    public void f()
+    {
+        Repository.Commit(Feature, Model.Description(1));
+
+        using var process = new Process();
+
+        process.StartInfo.FileName = @$".\{nameof(ConventionalReleaseNotes)}.exe";
+        process.StartInfo.Arguments = Repository.Path();
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+        var output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        output.Should().Be(Model.Changelog.Empty.WithGroup(Feature, 1) + NewLine);
+    }
+}
--- src/ConventionalReleaseNotes/Program.cs
+++ src/ConventionalReleaseNotes/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ConventionalReleaseNotes;
+
+var changelog = Changelog.FromRepository(args[0]);
+
+Console.WriteLine(changelog);
