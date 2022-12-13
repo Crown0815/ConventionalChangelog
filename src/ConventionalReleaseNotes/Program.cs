a26f28c61cb510edefab35e56655fa38b7bb6975
Felix Kröner <kroener@dynamic-biosensors.com>
2022-12-13 21:33:35 +0100

    refactor: Remove comment from Program.cs
    #SDPI-11

--- src/ConventionalReleaseNotes/Program.cs
+++ src/ConventionalReleaseNotes/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using ConventionalReleaseNotes;
+﻿using ConventionalReleaseNotes;
 
 var changelog = Changelog.FromRepository(args[0]);
-
 Console.WriteLine(changelog);
