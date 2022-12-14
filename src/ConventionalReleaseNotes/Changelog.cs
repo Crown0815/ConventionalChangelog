594ba1b3b4ac61e269194d5b785d96e1046c3edf
Felix Kröner <kroener@dynamic-biosensors.com>
2022-12-14 16:46:38 +0100

    refactor: Move fixup handling into dedicated extensions class
    #SDPI-11

--- src/ConventionalReleaseNotes/Changelog.cs
+++ src/ConventionalReleaseNotes/Changelog.cs
@@ -21,27 +21,6 @@ public static class Changelog
             .Aggregate(new LogAggregate(), Add).ToString();
     }
 
-    private static IEnumerable<CommitMessage> Reduce(this IEnumerable<CommitMessage> messages)
-    {
-        var relevant = new List<CommitMessage>();
-        var fixUps = new Dictionary<string, List<CommitMessage>>();
-        foreach (var message in messages)
-        {
-            relevant.Add(message);
-            foreach (var target in message.Footers.Where(x => x.Token == @"fixup").Select(x => x.Value))
-            {
-                if (!fixUps.ContainsKey(target))
-                    fixUps.Add(target, new List<CommitMessage>());
-                fixUps[target].Add(message);
-            }
-
-            if (fixUps.TryGetValue(message.Hash, out var ms))
-                relevant.RemoveAll(ms.Contains);
-        }
-
-        return relevant;
-    }
-
     private static LogAggregate Add(LogAggregate a, LogEntry l) => a.Add(l.Type, l.Description);
 
     private static IEnumerable<LogEntry> LogEntries(CommitMessage commitMessage)
