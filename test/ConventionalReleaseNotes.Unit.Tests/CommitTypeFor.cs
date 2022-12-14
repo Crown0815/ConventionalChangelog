5a4034685c41265218889ee40f5e146bb8f6671d
Felix Kröner <kroener@dynamic-biosensors.com>
2022-12-14 14:02:55 +0100

    refactor: Create irrelevant commit type for tests
    #SDPI-11

--- test/ConventionalReleaseNotes.Unit.Tests/CommitTypeFor.cs
+++ test/ConventionalReleaseNotes.Unit.Tests/CommitTypeFor.cs
@@ -8,6 +8,8 @@ internal static class CommitTypeFor
     public static readonly CommitType Bugfix = new("fix", "Bug Fixes");
     public static readonly CommitType PerformanceImprovement = new("perf", "Performance Improvements");
 
+    public static readonly CommitType Irrelevant = new("chore", "", Relevance.Hide);
+
     public static CommitType ToCommitType(this string indicator) => new(indicator, "");
 
     private const string BreakingChangeIndicator = "!";
