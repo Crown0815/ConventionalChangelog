89ed68ef634820d74f157510a5b6fba8a80c2b15
Felix Kröner <kroener@dynamic-biosensors.com>
2022-12-15 10:58:14 +0100

    refactor: Move method above method called by it
    #SDPI-11

--- test/ConventionalReleaseNotes.Unit.Tests/CommitCreationExtensions.cs
+++ test/ConventionalReleaseNotes.Unit.Tests/CommitCreationExtensions.cs
@@ -11,9 +11,9 @@ internal static class CommitCreationExtensions
     public static string CommitWith(this CommitType type, string description) =>
         $"{type.Indicator}: {description}";
 
-    public static string WithFooter(this string commitMessage, string token, string value) =>
-        commitMessage + Environment.NewLine + Environment.NewLine + $"{token}: {value}";
-
     public static string WithFooter(this string commitMessage, string token, int seed) =>
         commitMessage.WithFooter(token, A.Description(seed));
+
+    public static string WithFooter(this string commitMessage, string token, string value) =>
+        commitMessage + Environment.NewLine + Environment.NewLine + $"{token}: {value}";
 }
