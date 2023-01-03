b6afdd8a81d5bbc7582942198c29d29b8ee4ce08
Felix Kröner <kroener@dynamic-biosensors.com>
2023-01-03 11:02:15 +0100

    refactor: Extract methods to reduce duplication
    #SDPI-11

--- src/ConventionalChangelog/MessageLinq.cs
+++ src/ConventionalChangelog/MessageLinq.cs
@@ -5,42 +5,39 @@ namespace ConventionalChangelog;
 internal static class MessageLinq
 {
     public static IEnumerable<CommitMessage> Reduce(this IEnumerable<CommitMessage> m) => m
-        .ResolveFixUps()
-        .ResolveReverts();
+        .Resolve(FixUp)
+        .Resolve(Revert)
+        .Resolve(Override);
 
-    private static IEnumerable<CommitMessage> ResolveFixUps(this IEnumerable<CommitMessage> messages)
+    private static IEnumerable<CommitMessage> Resolve(this IEnumerable<CommitMessage> messages, Action<IDictionary<string, List<CommitMessage>>, CommitMessage, List<CommitMessage>> strategy)
     {
         var relevant = new List<CommitMessage>();
         var fixUps = new Dictionary<string, List<CommitMessage>>();
-        foreach (var message in messages)
-        {
-            if (fixUps.TryGetValue(message.Hash, out var ms))
-                relevant.RemoveAll(ms.Contains);
-
-            relevant.Add(message);
-            foreach (var target in message.Footers.Where(x => x.Token == @"fixup").Select(x => x.Value))
-            {
-                if (!fixUps.ContainsKey(target))
-                    fixUps.Add(target, new List<CommitMessage>());
-                fixUps[target].Add(message);
-            }
-        }
+        foreach (var message in messages) strategy(fixUps, message, relevant);
 
         return relevant;
     }
 
-    private static IEnumerable<CommitMessage> ResolveReverts(this IEnumerable<CommitMessage> messages)
+    private static void FixUp(IDictionary<string, List<CommitMessage>> fixUps, CommitMessage message, List<CommitMessage> relevant)
     {
-        var relevant = new List<CommitMessage>();
-        var reverts = new Dictionary<string, List<CommitMessage>>();
-        foreach (var message in messages)
-        {
-            if (reverts.TryGetValue(message.Hash, out var reverting))
-            {
-                relevant.RemoveAll(reverting.Contains);
-                continue;
-            }
+        if (fixUps.TryGetValue(message.Hash, out var ms))
+            relevant.RemoveAll(ms.Contains);
 
+        relevant.Add(message);
+        foreach (var target in message.Footers.Where(x => x.Token == @"fixup").Select(x => x.Value))
+        {
+            if (!fixUps.ContainsKey(target))
+                fixUps.Add(target, new List<CommitMessage>());
+            fixUps[target].Add(message);
+        }
+    }
+
+    private static void Revert(IDictionary<string, List<CommitMessage>> reverts, CommitMessage message, List<CommitMessage> relevant)
+    {
+        if (reverts.TryGetValue(message.Hash, out var reverting))
+            relevant.RemoveAll(reverting.Contains);
+        else
+        {
             relevant.Add(message);
             foreach (var target in message.Footers.Where(x => x.Token == @"revert").Select(x => x.Value))
             {
@@ -49,32 +46,18 @@ internal static class MessageLinq
                 reverts[target].Add(message);
             }
         }
-
-        return relevant;
     }
 
-    private static IEnumerable<CommitMessage> ResolveOverrides(this IEnumerable<CommitMessage> messages)
+    private static void Override(IDictionary<string, List<CommitMessage>> reverts, CommitMessage message, List<CommitMessage> relevant)
     {
-        var relevant = new List<CommitMessage>();
-        var reverts = new Dictionary<string, List<CommitMessage>>();
-        foreach (var message in messages)
-        {
-
+        if (!reverts.TryGetValue(message.Hash, out _))
             relevant.Add(message);
 
-            if (reverts.TryGetValue(message.Hash, out _))
-            {
-                relevant.Remove(message);
-            }
-
-            foreach (var target in message.Footers.Where(x => x.Token == @"override").Select(x => x.Value))
-            {
-                if (!reverts.ContainsKey(target))
-                    reverts.Add(target, new List<CommitMessage>());
-                reverts[target].Add(message);
-            }
+        foreach (var target in message.Footers.Where(x => x.Token == @"override").Select(x => x.Value))
+        {
+            if (!reverts.ContainsKey(target))
+                reverts.Add(target, new List<CommitMessage>());
+            reverts[target].Add(message);
         }
-
-        return relevant;
     }
 }
