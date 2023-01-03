aed4f3dab8d1ca1f7a9caa71679cfe28fc3c8b7f
Felix Kröner <kroener@dynamic-biosensors.com>
2023-01-03 10:29:55 +0100

    feat: Add support for reverting previous commit messages
    If a commit has the `revert: <hash>` footer, it will revert the
    referenced commit, i.e., it will hide itself and the commit if both are
    part of the changelog.
    
    #SDPI-11

--- src/ConventionalChangelog/MessageLinq.cs
+++ src/ConventionalChangelog/MessageLinq.cs
@@ -5,7 +5,8 @@ namespace ConventionalChangelog;
 internal static class MessageLinq
 {
     public static IEnumerable<CommitMessage> Reduce(this IEnumerable<CommitMessage> m) => m
-        .ResolveFixUps();
+        .ResolveFixUps()
+        .ResolveReverts();
 
     private static IEnumerable<CommitMessage> ResolveFixUps(this IEnumerable<CommitMessage> messages)
     {
@@ -23,6 +24,30 @@ internal static class MessageLinq
 
             if (fixUps.TryGetValue(message.Hash, out var ms))
                 relevant.RemoveAll(ms.Contains);
+        }
+
+        return relevant;
+    }
+
+    private static IEnumerable<CommitMessage> ResolveReverts(this IEnumerable<CommitMessage> messages)
+    {
+        var relevant = new List<CommitMessage>();
+        var reverts = new Dictionary<string, List<CommitMessage>>();
+        foreach (var message in messages)
+        {
+            if (reverts.TryGetValue(message.Hash, out var reverting))
+            {
+                relevant.RemoveAll(reverting.Contains);
+                continue;
+            }
+
+            relevant.Add(message);
+            foreach (var target in message.Footers.Where(x => x.Token == @"revert").Select(x => x.Value))
+            {
+                if (!reverts.ContainsKey(target))
+                    reverts.Add(target, new List<CommitMessage>());
+                reverts[target].Add(message);
+            }
         }
 
         return relevant;
---
+++ test/ConventionalChangelog.Unit.Tests/Changelog_specs/A_changelog_from_changelog_relevant_conventional_commits_with_reverting_commits.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ConventionalChangelog.Unit.Tests.Changelog_specs;
+
+public partial class A_changelog_from_changelog_relevant_conventional_commits
+{
+    public class With_reverting_commits
+    {
+        private const string RevertToken = @"revert";
+        private readonly Commit _reverted;
+        private readonly Commit _reverting;
+
+        public With_reverting_commits()
+        {
+            _reverted = CommitTypeFor.Feature.CommitWithDescription(1);
+            _reverting = CommitTypeFor.Feature.CommitWithDescription(2).WithFooter(RevertToken, _reverted.Hash);
+        }
+
+        [Fact]
+        public void when_the_reverted_commit_is_part_of_the_changelog_but_the_reverting_one_is_not_shows_the_reverted_commit()
+        {
+            var changelog = Changelog.From(_reverted);
+            changelog.Should().Be(A.Changelog.WithGroup(CommitTypeFor.Feature, 1));
+        }
+
+        [Fact]
+        public void when_the_reverted_and_reverting_commit_are_part_of_the_changelog_shows_neither()
+        {
+            var changelog = Changelog.From(_reverting, _reverted);
+
+            changelog.Should().Be(A.Changelog.Empty);
+        }
+
+        [Fact]
+        public void when_the_reverting_commit_is_part_of_the_changelog_but_the_reverted_one_is_not_shows_the_reverting_commit()
+        {
+            var changelog = Changelog.From(_reverting);
+            changelog.Should().Be(A.Changelog.WithGroup(CommitTypeFor.Feature, 2));
+        }
+
+        [Fact]
+        public void when_a_reverted_reverting_commit_is_part_of_the_changelog_shows_the_originally_reverted_commit()
+        {
+            var revertingReverting =
+                CommitTypeFor.Feature.CommitWithDescription(3).WithFooter(RevertToken, _reverting.Hash);
+
+            var changelog = Changelog.From(revertingReverting, _reverting, _reverted);
+
+            changelog.Should().Be(A.Changelog.WithGroup(CommitTypeFor.Feature, 1));
+        }
+
+        [Fact]
+        public void when_multiple_reverting_commits_target_a_single_commit_that_is_part_of_the_changelog_shows_none_of_these_commits()
+        {
+            var reverting2 = CommitTypeFor.Feature.CommitWithDescription(3).WithFooter(RevertToken, _reverted.Hash);
+
+            var changelog = Changelog.From(reverting2, _reverting, _reverted);
+
+            changelog.Should().Be(A.Changelog.Empty);
+        }
+    }
+}
