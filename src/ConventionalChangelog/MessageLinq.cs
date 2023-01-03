55ee19568c6b98384e36c1debbccf3df82ac9e1d
Felix Kröner <kroener@dynamic-biosensors.com>
2023-01-03 11:36:06 +0100

    refactor: Replace logic duplication with lookup strategy
    #SDPI-11

--- src/ConventionalChangelog/MessageLinq.cs
+++ src/ConventionalChangelog/MessageLinq.cs
@@ -4,27 +4,37 @@ namespace ConventionalChangelog;
 
 internal static class MessageLinq
 {
+    private static readonly Strategy FixUp = new(@"fixup", true, true, true);
+    private static readonly Strategy Revert = new("revert", true, false, false);
+    private static readonly Strategy Override = new("override", false, false, true);
+
     public static IEnumerable<CommitMessage> Reduce(this IEnumerable<CommitMessage> m) => m
         .Resolve(FixUp)
         .Resolve(Revert)
         .Resolve(Override);
 
-    private static IEnumerable<CommitMessage> Resolve(this IEnumerable<CommitMessage> messages, Action<IDictionary<string, List<CommitMessage>>, CommitMessage, List<CommitMessage>> strategy)
+    private static IEnumerable<CommitMessage> Resolve(this IEnumerable<CommitMessage> messages, Strategy strategy)
     {
-        var relevant = new List<CommitMessage>();
-        var fixUps = new Dictionary<string, List<CommitMessage>>();
-        foreach (var message in messages) strategy(fixUps, message, relevant);
+        var resolved = new List<CommitMessage>();
+        var references = new Dictionary<string, List<CommitMessage>>();
+        foreach (var message in messages) Reduce(strategy, references, message, resolved);
 
-        return relevant;
+        return resolved;
     }
 
-    private static void FixUp(IDictionary<string, List<CommitMessage>> fixUps, CommitMessage message, List<CommitMessage> relevant)
+    private static void Reduce(Strategy strategy, IDictionary<string, List<CommitMessage>> references, CommitMessage message,
+        List<CommitMessage> relevant)
     {
-        if (fixUps.TryGetValue(message.Hash, out var ms))
-            relevant.RemoveAll(ms.Contains);
+        var found = references.TryGetValue(message.Hash, out var ms);
 
-        relevant.Add(message);
-        foreach (var target in message.Footers.Where(x => x.Token == @"fixup").Select(x => x.Value))
+        if (!found || strategy.Add) relevant.Add(message);
+        if (!found || strategy.Register) Register(references, message, strategy.Token);
+        if (found && strategy.Remove) relevant.RemoveAll(ms!.Contains);
+    }
+
+    private static void Register(IDictionary<string, List<CommitMessage>> fixUps, CommitMessage message, string token)
+    {
+        foreach (var target in message.Footers.Where(x => x.Token == token).Select(x => x.Value))
         {
             if (!fixUps.ContainsKey(target))
                 fixUps.Add(target, new List<CommitMessage>());
@@ -32,32 +42,5 @@ internal static class MessageLinq
         }
     }
 
-    private static void Revert(IDictionary<string, List<CommitMessage>> reverts, CommitMessage message, List<CommitMessage> relevant)
-    {
-        if (reverts.TryGetValue(message.Hash, out var reverting))
-            relevant.RemoveAll(reverting.Contains);
-        else
-        {
-            relevant.Add(message);
-            foreach (var target in message.Footers.Where(x => x.Token == @"revert").Select(x => x.Value))
-            {
-                if (!reverts.ContainsKey(target))
-                    reverts.Add(target, new List<CommitMessage>());
-                reverts[target].Add(message);
-            }
-        }
-    }
-
-    private static void Override(IDictionary<string, List<CommitMessage>> reverts, CommitMessage message, List<CommitMessage> relevant)
-    {
-        if (!reverts.TryGetValue(message.Hash, out _))
-            relevant.Add(message);
-
-        foreach (var target in message.Footers.Where(x => x.Token == @"override").Select(x => x.Value))
-        {
-            if (!reverts.ContainsKey(target))
-                reverts.Add(target, new List<CommitMessage>());
-            reverts[target].Add(message);
-        }
-    }
+    private record Strategy(string Token, bool Remove, bool Add, bool Register);
 }
