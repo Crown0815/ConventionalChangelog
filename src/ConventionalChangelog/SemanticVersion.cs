2f2e0388cf2a86d983e51ad42c8ecfbe716b379d
Felix Kröner <felix.kroener@dynamic-biosensors.com>
2023-10-27 20:49:36 +0200

    refactor: Resolve partial class with single part warning in .NET6

--- src/ConventionalChangelog/SemanticVersion.cs
+++ src/ConventionalChangelog/SemanticVersion.cs
@@ -2,17 +2,19 @@ using System.Text.RegularExpressions;
 
 namespace ConventionalChangelog;
 
-internal static partial class SemanticVersion
-{
-    // language=regex
-    private const string SemanticVersionPattern = @"([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$";
-
 #if NET6_0
+internal static class SemanticVersion
+{
     private static Regex SemanticVersionRegex() => new(SemanticVersionPattern);
 #elif NET7_0_OR_GREATER
+internal static partial class SemanticVersion
+{
     [GeneratedRegex(SemanticVersionPattern)]
     private static partial Regex SemanticVersionRegex();
 #endif
+
+    // language=regex
+    private const string SemanticVersionPattern = @"([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$";
 
     private static readonly Lazy<Regex> Regex = new(SemanticVersionRegex);
 
