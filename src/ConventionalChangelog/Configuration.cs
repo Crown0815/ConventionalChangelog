8a7fc118358af792cbc64e780b532de5065ec315
Felix Kröner <felix.kroener@dynamic-biosensors.com>
2023-10-31 15:06:44 +0100

    refactor: Extract defaults into default configuration

--- src/ConventionalChangelog/Configuration.cs
+++ src/ConventionalChangelog/Configuration.cs
@@ -4,39 +4,13 @@ namespace ConventionalChangelog;
 
 public class Configuration
 {
-    // language=regex
-    private const string BreakingChangeTokenPattern = "(?<breaking>(?<token>BREAKING[ -]CHANGE))(: | #)";
-    // language=regex
-    private const string TrailerTokenPattern = @"(?<token>[\w\-]+)(: | #)";
-    // language=regex
-    private const string YouTrackTokenPattern = @"#(?<token>\w+-\d+)";
+    public string FooterPattern => DefaultConfiguration.FooterPattern;
 
-    private const string CompleteFooterPattern = $"^{BreakingChangeTokenPattern}|{TrailerTokenPattern}|{YouTrackTokenPattern}";
+    public string VersionTagPrefix => DefaultConfiguration.VersionTagPrefix;
 
-    // language=regex
-    private const string SemanticVersionPattern2 = @"([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?";
+    public string SemanticVersionPattern => DefaultConfiguration.SemanticVersionPattern;
 
-    public string FooterPattern => CompleteFooterPattern;
+    public IEnumerable<CommitType> CommitTypes => DefaultConfiguration.CommitTypes;
 
-    // language=regex
-    public string VersionTagPrefix => "[pv]";
-
-    public string SemanticVersionPattern => SemanticVersionPattern2;
-
-    public CommitType[] CommitTypes { get; } =
-    {
-        new("(?<inner>[a-z]+)!", "Breaking Changes", Relevance.Show),
-        new("feat", "Features", Relevance.Show),
-        new("fix", "Bug Fixes", Relevance.Show),
-        new("perf", "Performance Improvements", Relevance.Show),
-        new("build", "", Relevance.Hide),
-        new("chore", "", Relevance.Hide),
-        new("ci", "", Relevance.Hide),
-        new("docs", "", Relevance.Hide),
-        new("style", "", Relevance.Hide),
-        new("refactor", "", Relevance.Hide),
-        new("test", "", Relevance.Hide),
-    };
-
-    public ChangelogOrder ChangelogOrder { get; init; }
+    public ChangelogOrder ChangelogOrder { get; init; } = DefaultConfiguration.ChangelogOrder;
 }
---
+++ src/ConventionalChangelog/DefaultConfiguration.cs
@@ -0,0 +1,36 @@
+using ConventionalChangelog.Conventional;
+
+namespace ConventionalChangelog;
+
+internal static class DefaultConfiguration
+{
+    // language=regex
+    private const string BreakingChangeTokenPattern = "(?<breaking>(?<token>BREAKING[ -]CHANGE))(: | #)";
+    // language=regex
+    private const string TrailerTokenPattern = @"(?<token>[\w\-]+)(: | #)";
+    // language=regex
+    private const string YouTrackTokenPattern = @"#(?<token>\w+-\d+)";
+    // language=regex
+    public const string FooterPattern = $"^{BreakingChangeTokenPattern}|{TrailerTokenPattern}|{YouTrackTokenPattern}";
+    // language=regex
+    public const string VersionTagPrefix = "[pv]";
+    // language=regex
+    public const string SemanticVersionPattern = @"([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?";
+
+    public const ChangelogOrder ChangelogOrder = default;
+
+    public static readonly CommitType[] CommitTypes =
+    {
+        new("(?<inner>[a-z]+)!", "Breaking Changes", Relevance.Show),
+        new("feat", "Features", Relevance.Show),
+        new("fix", "Bug Fixes", Relevance.Show),
+        new("perf", "Performance Improvements", Relevance.Show),
+        new("build", "", Relevance.Hide),
+        new("chore", "", Relevance.Hide),
+        new("ci", "", Relevance.Hide),
+        new("docs", "", Relevance.Hide),
+        new("style", "", Relevance.Hide),
+        new("refactor", "", Relevance.Hide),
+        new("test", "", Relevance.Hide),
+    };
+}
