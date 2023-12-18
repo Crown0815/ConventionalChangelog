01cca9c351da2c65111a609a4d601f247b6d0e72
Felix Kröner <felix.kroener@dynamic-biosensors.com>
2023-12-18 16:17:14 +0100

    refactor: Separate path and repository state from reader

--- src/ConventionalChangelog/RepositoryReader.cs
+++ src/ConventionalChangelog/RepositoryReader.cs
@@ -13,51 +13,84 @@ internal class RepositoryReader
 
     public IEnumerable<Commit> CommitsFrom(string path)
     {
-        using var repository = new Repository(path);
-        var newestVersionTag = NewestVersionCommitIn(repository);
-
-        return repository.Commits
-            .QueryBy(AllSince(newestVersionTag))
-            .Select(c => AsCommit(c, path))
-            .ToArray();
+        using var repository = new RepositoryWrapper(path);
+        return repository.AllCommitsSinceLastTagMatching(_customization.IsVersionTag);
     }
 
-    private object? NewestVersionCommitIn(IRepository repository)
-    {
-        var versionCommits = repository.Tags
-            .Where(tag => _customization.IsVersionTag(tag.FriendlyName))
-            .Select(x => x.Target)
-            .ToHashSet();
 
-        return repository.Commits
-            .QueryBy(MainlineFrom(repository.Head))
-            .FirstOrDefault(versionCommits.Contains);
-    }
-
-    private static CommitFilter MainlineFrom(Branch anchor) => new()
+    private class RepositoryWrapper : IDisposable
     {
-        SortBy = CommitSortStrategies.Topological,
-        FirstParentOnly = true,
-        IncludeReachableFrom = anchor,
-    };
+        private readonly string _path;
+        private readonly IRepository _inner;
 
-    private static CommitFilter AllSince(object? anchor) => new()
-    {
-        SortBy = CommitSortStrategies.Topological,
-        ExcludeReachableFrom = anchor,
-    };
+        public RepositoryWrapper(string path)
+        {
+            _path = path;
+            _inner = new Repository(path);
+        }
 
-    private static Commit AsCommit(LibGit2Sharp.Commit commit, string path)
-    {
-        var message = MessageFor(commit, path);
-        return new Commit(message, commit.Sha);
-    }
+        public IEnumerable<Commit> AllCommitsSinceLastTagMatching(Func<string, bool> condition)
+        {
+            var lastTaggedCommit = FirstCommitFromHeadWithTagMatching(condition);
+            return AllCommitsSince(lastTaggedCommit);
+        }
 
-    private static string MessageFor(LibGit2Sharp.Commit c, string path)
-    {
-        var overwrite = Path.Combine(path, ".conventional-changelog", c.Sha);
-        if (File.Exists(overwrite))
-            return File.ReadAllText(overwrite);
-        return c.Message;
+        private object? FirstCommitFromHeadWithTagMatching(Func<string, bool> condition)
+        {
+            var taggedCommits = CommitsWithTagMatching(condition);
+            return FirstCommitFromHeadContainedIn(taggedCommits);
+        }
+
+        private HashSet<GitObject> CommitsWithTagMatching(Func<string, bool> predicate)
+        {
+            return _inner.Tags
+                .Where(t => predicate(t.FriendlyName))
+                .Select(t => t.Target)
+                .ToHashSet();
+        }
+
+        private LibGit2Sharp.Commit? FirstCommitFromHeadContainedIn(IReadOnlySet<GitObject> commits)
+        {
+            return _inner.Commits
+                .QueryBy(MainlineFrom(_inner.Head))
+                .FirstOrDefault(commits.Contains);
+        }
+
+        private static CommitFilter MainlineFrom(Branch anchor) => new()
+        {
+            SortBy = CommitSortStrategies.Topological,
+            FirstParentOnly = true,
+            IncludeReachableFrom = anchor,
+        };
+
+        private IEnumerable<Commit> AllCommitsSince(object? referenceCommit)
+        {
+            return _inner.Commits
+                .QueryBy(AllSince(referenceCommit))
+                .Select(AsCommit)
+                .ToArray();
+        }
+
+        private static CommitFilter AllSince(object? anchor) => new()
+        {
+            SortBy = CommitSortStrategies.Topological,
+            ExcludeReachableFrom = anchor,
+        };
+
+        private Commit AsCommit(LibGit2Sharp.Commit commit)
+        {
+            var message = MessageFor(commit);
+            return new Commit(message, commit.Sha);
+        }
+
+        private string MessageFor(LibGit2Sharp.Commit c)
+        {
+            var overwrite = Path.Combine(_path, ".conventional-changelog", c.Sha);
+            if (File.Exists(overwrite))
+                return File.ReadAllText(overwrite);
+            return c.Message;
+        }
+
+        public void Dispose() => _inner.Dispose();
     }
 }
