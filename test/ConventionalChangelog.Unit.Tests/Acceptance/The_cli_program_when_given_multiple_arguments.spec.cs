Crown0815/ConventionalChangelog

commit c98dfc1781e1a179f0c20c3a4549b682e5cf9ec4

feat: Add support for overwriting the version pattern using `-t` or `--tag-prefix`

src/ConventionalChangelog/Program.cs
@@ -3,11 +3,17 @@ using ConventionalChangelog.BuildSystems;
 using ConventionalChangelog.Configuration;
 
 var output = (string?)null;
+var tagPrefix = (string?)null;
 var repositoryPath = args[^1];
 
 if (args[0] is "-o" or "--output") output = args[1];
+if (args[0] is "-t" or "--tag-prefix") tagPrefix = args[1];
 
-var configuration = new Configuration { ChangelogOrder = default};
+var configuration = new Configuration
+{
+    ChangelogOrder = default,
+    VersionTagPrefix = tagPrefix!,
+};
 var changelog = new Changelog(configuration).FromRepository(repositoryPath);
 
 if (output is not null)

src/ConventionalChangelog/Configuration/Configuration.cs
@@ -4,10 +4,15 @@ public class Configuration : IConfiguration
 {
     private readonly IConfiguration _default = new DefaultConfiguration();
     private readonly ChangelogOrder? _changelogOrder;
+    private readonly string? _versionTagPrefix;
 
     public string FooterPattern => _default.FooterPattern;
 
-    public string VersionTagPrefix => _default.VersionTagPrefix;
+    public string VersionTagPrefix
+    {
+        get => _versionTagPrefix ?? _default.VersionTagPrefix;
+        init => _versionTagPrefix = value;
+    }
 
     public string SemanticVersionPattern => _default.SemanticVersionPattern;
 

test/ConventionalChangelog.Unit.Tests/Acceptance/The_cli_program_when_given_a_version_tag_prefix.spec.cs
@@ -7,21 +7,51 @@ namespace ConventionalChangelog.Unit.Tests.Acceptance;
 
 public sealed class The_cli_program_when_given_a_version_tag_prefix : CliTestsBase
 {
-    private const string TagPrefixKeyShort = "-t";
-    private const string TagPrefixKeyLong = "--tag-prefix";
+    private const string SemanticVersion = "1.0.0";
+    public static string[] TagPrefixKeys { get; } = { "-t", "--tag-prefix" };
 
-    public static TheoryData<string> OutputKeys => new() { TagPrefixKeyShort, TagPrefixKeyLong };
-
-    [Theory]
-    [MemberData(nameof(OutputKeys))]
-    public void prints_changelog_into_the_file_given_through_the(string argument)
+    [Theory, CombinatorialData]
+    public void literal_prints_changelog_from_last_commit_with_matching_version_tag(
+        [CombinatorialMemberData(nameof(TagPrefixKeys))] string argument,
+        [CombinatorialValues('t', 'r', "ver")] string prefix
+    )
     {
         Repository.Commit(Feature, "Before tag");
-        Repository.Commit(Feature, "Tagged commit").Tag("p1.0.0-alpha.1");
+        Repository.Commit(Feature, "Tagged commit").Tag($"{prefix}{SemanticVersion}");
         Repository.Commit(Feature, 1);
 
-        var output = OutputWithInput($"{argument} [p] {Repository.Path()}");
+        var output = OutputWithInput($"{argument} {prefix} {Repository.Path()}");
 
         output.Should().Be(A.Changelog.WithGroup(Feature, 1) + NewLine);
+    }
+
+    [Theory, CombinatorialData]
+    public void regular_expression_prints_changelog_from_last_commit_with_matching_version_tag(
+        [CombinatorialMemberData(nameof(TagPrefixKeys))] string argument,
+        [CombinatorialValues('t', 'r', "ver")] string prefix
+    )
+    {
+        Repository.Commit(Feature, "Before tag");
+        Repository.Commit(Feature, "Tagged commit").Tag($"{prefix}{SemanticVersion}");
+        Repository.Commit(Feature, 1);
+
+        var output = OutputWithInput($"{argument} [tr]|ver {Repository.Path()}");
+
+        output.Should().Be(A.Changelog.WithGroup(Feature, 1) + NewLine);
+    }
+
+    [Theory, CombinatorialData]
+    public void prints_changelog_while_ignoring_non_matching_version_tags(
+        [CombinatorialMemberData(nameof(TagPrefixKeys))] string argument,
+        [CombinatorialValues('t', 'r', "ver", "[tr]")] string pattern
+    )
+    {
+        Repository.Commit(Feature, -1);
+        Repository.Commit(Feature, 0).Tag($"v{SemanticVersion}");
+        Repository.Commit(Feature, 1);
+
+        var output = OutputWithInput($"{argument} {pattern} {Repository.Path()}");
+
+        output.Should().Be(A.Changelog.WithGroup(Feature, 1, 0, -1) + NewLine);
     }
 }

commit ee87d6d6121eac26609886032151120cbe945098

fix: Fix multiple options not working as expected in cli
Fixes: c98dfc1781e1a179f0c20c3a4549b682e5cf9ec4

src/ConventionalChangelog/Program.cs
@@ -1,38 +1,34 @@
-﻿using ConventionalChangelog;
+﻿using Cocona;
+using ConventionalChangelog;
 using ConventionalChangelog.BuildSystems;
 using ConventionalChangelog.Configuration;
 
-var output = (string?)null;
-var tagPrefix = (string?)null;
-var ignorePrereleases = false;
-var repositoryPath = args[^1];
-
-if (args[0] is "-o" or "--output") output = args[1];
-if (args[0] is "-t" or "--tag-prefix") tagPrefix = args[1];
-if (args[0] is "-i" or "--ignore-prereleases") ignorePrereleases = true;
-
-var configuration = new Configuration
-{
-    ChangelogOrder = default,
-    VersionTagPrefix = tagPrefix!,
-    IgnorePrerelease = ignorePrereleases,
-};
-var changelog = new Changelog(configuration).FromRepository(repositoryPath);
-
-if (output is not null)
-{
-    File.WriteAllText(output, changelog + Environment.NewLine);
-    if (IsRunningInTeamCityCi())
-        Console.WriteLine(TeamCityParameterMessageFrom(changelog));
-}
-else
-{
-    Console.WriteLine(IsRunningInTeamCityCi()
-        ? TeamCityParameterMessageFrom(changelog)
-        : changelog);
-}
-
+CoconaLiteApp.Run(Execute);
 return;
+
+void Execute([Option('o')]string? output, [Option('t')]string? tagPrefix, [Option('i')]bool ignorePrereleases, [Argument]string repositoryPath)
+{
+    var configuration = new Configuration
+    {
+        ChangelogOrder = default,
+        VersionTagPrefix = tagPrefix!,
+        IgnorePrerelease = ignorePrereleases,
+    };
+    var changelog = new Changelog(configuration).FromRepository(repositoryPath);
+
+    if (output is not null)
+    {
+        File.WriteAllText(output, changelog + Environment.NewLine);
+        if (IsRunningInTeamCityCi())
+            Console.WriteLine(TeamCityParameterMessageFrom(changelog));
+    }
+    else
+    {
+        Console.WriteLine(IsRunningInTeamCityCi()
+            ? TeamCityParameterMessageFrom(changelog)
+            : changelog);
+    }
+}
 
 bool IsRunningInTeamCityCi()
 {

test/ConventionalChangelog.Unit.Tests/Acceptance/The_cli_program_when_given_multiple_arguments.spec.cs
@@ -1,0 +1,32 @@
+using System;
+using System.IO;
+using FluentAssertions;
+using Xunit;
+using static System.Environment;
+using static ConventionalChangelog.Unit.Tests.CommitTypeFor;
+
+namespace ConventionalChangelog.Unit.Tests.Acceptance;
+
+public sealed class The_cli_program_when_given_multiple_arguments : CliTestsBase
+{
+    private readonly string _fileName = Guid.NewGuid().ToString();
+
+    [Fact]
+    public void handles_each_of_them_individually()
+    {
+        Repository.Commit(Feature, 0).Tag("r1.0.0");
+        Repository.Commit(Feature, 1).Tag("r1.0.0-alpha");
+        Repository.Commit(Feature, 2);
+
+        var output = OutputWithInput($"-t r -o {_fileName} -i {Repository.Path()}");
+
+        output.Should().BeEmpty();
+        File.ReadAllText(_fileName).Should().Be(A.Changelog.WithGroup(Feature, 2, 1) + NewLine);
+    }
+
+    public override void Dispose()
+    {
+        File.Delete(_fileName);
+        base.Dispose();
+    }
+}