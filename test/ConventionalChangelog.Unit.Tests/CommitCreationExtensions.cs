b99aa93ae5e781e7d5d2cbe07b83e2b8da94d84e
Felix Kröner <felix.kroener@dynamic-biosensors.com>
2024-11-17 12:45:06 +0100

    refactor: Extract constants and magic strings/chars

--- test/ConventionalChangelog.Unit.Tests/CommitCreationExtensions.cs
+++ test/ConventionalChangelog.Unit.Tests/CommitCreationExtensions.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using static System.Environment;
 
 namespace ConventionalChangelog.Unit.Tests;
 
 internal static class CommitCreationExtensions
 {
+    private const char TypeDescriptionSeparator = ':';
+    private const char TokenValueSeparator = TypeDescriptionSeparator;
+    private static readonly string BlankLine = NewLine + NewLine;
+
     public static Commit CommitWithDescription(this CommitType type, int seed) =>
         type.CommitWith(A.Description(seed));
 
     public static Commit CommitWith(this CommitType type, string description) =>
-        new($"{type.Indicator}: {description}");
+        new($"{type.Indicator}{TypeDescriptionSeparator} {description}");
 
     public static Commit WithFooter(this Commit commitMessage, string token, int seed) =>
         commitMessage.WithFooter(token, A.Description(seed));
 
     public static Commit WithFooter(this Commit commitMessage, string token, string value) =>
         commitMessage with {Message = commitMessage.Message + Environment.NewLine + Environment.NewLine + $"{token}: {value}"};
+        commitMessage with {Message = commitMessage.Message + BlankLine + $"{token}{TokenValueSeparator} {value}"};
 }
