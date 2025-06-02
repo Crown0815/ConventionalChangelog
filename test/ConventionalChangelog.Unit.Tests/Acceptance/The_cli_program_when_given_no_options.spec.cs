fd4a32d0f458031f0ad73e79ed8d988108a2c23f
Crown0815 <felix.kroener@gmx.de>
2025-06-02 20:35:29 +0200

    refactor: Split teamcity and non-ci tests

--- test/ConventionalChangelog.Unit.Tests/Acceptance/The_cli_program_when_given_no_options.spec.cs
+++ test/ConventionalChangelog.Unit.Tests/Acceptance/The_cli_program_when_given_no_options.spec.cs
@@ -1,12 +1,11 @@
-﻿using ConventionalChangelog.BuildSystems;
-using AwesomeAssertions;
+﻿using AwesomeAssertions;
 using Xunit;
 using static System.Environment;
 using static ConventionalChangelog.Unit.Tests.CommitTypeFor;
 
 namespace ConventionalChangelog.Unit.Tests.Acceptance;
 
-public class The_cli_program_when_given_no_options : CliTestsBase
+public class The_cli_program_when_outside_of_ci_context : CliTestsBase
 {
     [Fact]
     public void prints_the_changelog_from_a_given_repository_to_the_console()
@@ -16,15 +15,5 @@ public class The_cli_program_when_given_no_options : CliTestsBase
         var output = OutputWithInput(Repository.Path());
 
         output.Should().Be(A.Changelog.WithGroup(Feature, 1) + NewLine);
-    }
-
-    [Fact]
-    public void run_from_teamcity_prints_a_service_message_setting_a_parameter_to_the_changelog()
-    {
-        Repository.Commit(Feature, 1);
-
-        var output = OutputWithInput(Repository.Path(), (TeamCity.EnvironmentVariable, "whatever"));
-
-        output.Should().Be(TeamCity.SetParameterCommand("CRN.Changelog", A.Changelog.WithGroup(Feature, 1)) + NewLine);
     }
 }
