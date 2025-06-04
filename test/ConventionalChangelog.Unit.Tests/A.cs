f3b0168b90a56998ca3d05b9794c344fad9785fe
Crown0815 <felix.kroener@gmx.de>
2025-06-04 15:30:46 +0200

    refactor: Reorganize changelog builder in tests to avoid static methods

--- test/ConventionalChangelog.Unit.Tests/A.cs
+++ test/ConventionalChangelog.Unit.Tests/A.cs
@@ -5,7 +5,9 @@ namespace ConventionalChangelog.Unit.Tests;
 
 internal static class A
 {
-    internal class Changelog
+    internal static ChangelogBuilder Changelog => new();
+
+    internal class ChangelogBuilder
     {
         private const string ChangelogTitle = "# Changelog";
         private const string GeneralCodeImprovementsMessage = "*General Code Improvements*";
@@ -16,54 +18,62 @@ internal static class A
 
         private string _text = ChangelogTitle + NewLine;
 
-        public static Changelog Empty => new();
+        public ChangelogBuilder Empty => this;
 
-        public static Changelog WithoutTitle()
+        public ChangelogBuilder WithoutTitle()
         {
             var changelog = Empty;
             changelog._text = "";
             return changelog;
         }
 
-        public static string WithGeneralCodeImprovementsMessage() =>
+        public string WithGeneralCodeImprovementsMessage() =>
             Empty.With(NewLine + GeneralCodeImprovementsMessage);
 
-        public static Changelog WithGroup(ChangelogType type, params int[] seeds) =>
+        public ChangelogBuilder WithGroup(ChangelogType type, params int[] seeds) =>
             Empty.And(type, seeds);
 
-        public static Changelog WithGroup(ChangelogType type, string scope, params int[] seeds) =>
+        public ChangelogBuilder WithGroup(ChangelogType type, string scope, params int[] seeds) =>
             Empty.And(type, scope, seeds);
 
-        public Changelog And(ChangelogType type, params int[] seeds)
+        public string WithGroup(CommitType type, params (int, string)[] message)
+        {
+            return message.Aggregate(WithGroup(type.GroupHeader), AddBulletPoint);
+        }
+
+        public ChangelogBuilder And(ChangelogType type, params int[] seeds)
         {
             return seeds.Aggregate(WithGroup(type.GroupHeader), AddBulletPoint);
         }
 
-        public Changelog And(ChangelogType type, string scope, params int[] seeds)
+        public ChangelogBuilder And(ChangelogType type, string scope, params int[] seeds)
         {
             return seeds.Aggregate(WithGroup(type.GroupHeader).WithScope(scope), AddBulletPoint);
         }
 
-        private Changelog WithGroup(string groupHeader) =>
+        private ChangelogBuilder WithGroup(string groupHeader) =>
             With(NewLine + string.Format(HeaderL2, groupHeader) + HeaderSeparator);
 
-        private Changelog WithScope(string scopeHeader) =>
+        private ChangelogBuilder WithScope(string scopeHeader) =>
             With(string.Format(HeaderL3, scopeHeader) + HeaderSeparator);
 
-        public Changelog AndScope(string scopeHeader, params int[] seeds) =>
+        public ChangelogBuilder AndScope(string scopeHeader, params int[] seeds) =>
             seeds.Aggregate(With(NewLine).WithScope(scopeHeader), AddBulletPoint);
 
-        private static Changelog AddBulletPoint(Changelog c, int seed) => c.WithBulletPoint(seed);
+        private static ChangelogBuilder AddBulletPoint(ChangelogBuilder c, int seed) => c.WithBulletPoint(seed);
 
-        private Changelog WithBulletPoint(int seed) => With($"- {Description(seed)}{NewLine}");
+        private static ChangelogBuilder AddBulletPoint(ChangelogBuilder c, (int seed, string hash) m) => c.WithBulletPoint(m.seed, m.hash);
 
-        private Changelog With(string text)
+        private ChangelogBuilder WithBulletPoint(int seed) => With($"- {Description(seed)}{NewLine}");
+        private ChangelogBuilder WithBulletPoint(int seed, string hash) => With($"- {Description(seed)} ({hash}){NewLine}");
+
+        private ChangelogBuilder With(string text)
         {
             _text += text;
             return this;
         }
 
-        public static implicit operator string(Changelog x) => x._text;
+        public static implicit operator string(ChangelogBuilder x) => x._text;
     }
 
     public static string Description(int seed) => $"Some Description {seed}";
