182a923d5e94e80f662fd189d02cc80d96c0374f
Crown0815 <felix.kroener@gmx.de>
2025-06-05 09:54:40 +0200

    refactor: Extract breaking change defining constants

--- test/ConventionalChangelog.Unit.Tests/Constants.spec.cs
+++ test/ConventionalChangelog.Unit.Tests/Constants.spec.cs
@@ -5,6 +5,12 @@ namespace ConventionalChangelog.Unit.Tests;
 
 public class The_constant
 {
+    public static readonly TheoryData<string> BreakingChangeFooterTokens =
+    [
+        "BREAKING CHANGE",
+        "BREAKING-CHANGE",
+    ];
+
     [Fact]
     public void output_changelog_has_the_expected_value()
     {
