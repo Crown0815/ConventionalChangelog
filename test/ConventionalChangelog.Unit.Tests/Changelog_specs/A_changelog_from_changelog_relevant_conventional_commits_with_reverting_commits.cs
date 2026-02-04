4840ac0470c3ed34818a49835c9b650be19135fa
Crown0815 <felix.kroener@gmx.de>
2026-02-04 13:25:48 +0100

    test: Ensure that overriding and reverting multiple commits works

--- test/ConventionalChangelog.Unit.Tests/Changelog_specs/A_changelog_from_changelog_relevant_conventional_commits_with_reverting_commits.cs
+++ test/ConventionalChangelog.Unit.Tests/Changelog_specs/A_changelog_from_changelog_relevant_conventional_commits_with_reverting_commits.cs
@@ -1,5 +1,6 @@
 ﻿using AwesomeAssertions;
 using Xunit;
+using static ConventionalChangelog.Unit.Tests.CommitTypeFor;
 
 namespace ConventionalChangelog.Unit.Tests.Changelog_specs;
 
@@ -13,15 +14,15 @@ public partial class A_changelog_from_changelog_relevant_conventional_commits
 
         public With_reverting_commits()
         {
-            _reverted = CommitTypeFor.Feature.CommitWithDescription(1);
-            _reverting = CommitTypeFor.Feature.CommitWithDescription(2).WithFooter(DefaultRevertToken, _reverted.Hash);
+            _reverted = Feature.CommitWithDescription(1);
+            _reverting = Feature.CommitWithDescription(2).WithFooter(DefaultRevertToken, _reverted.Hash);
         }
 
         [Fact]
         public void when_the_reverted_commit_is_part_of_the_changelog_but_the_reverting_one_is_not_shows_the_reverted_commit()
         {
             var changelog = The.ChangelogFrom(_reverted);
-            changelog.Should().Be(A.Changelog.WithGroup(CommitTypeFor.Feature, 1));
+            changelog.Should().Be(A.Changelog.WithGroup(Feature, 1));
         }
 
         [Fact]
@@ -36,24 +37,24 @@ public partial class A_changelog_from_changelog_relevant_conventional_commits
         public void when_the_reverting_commit_is_part_of_the_changelog_but_the_reverted_one_is_not_shows_the_reverting_commit()
         {
             var changelog = The.ChangelogFrom(_reverting);
-            changelog.Should().Be(A.Changelog.WithGroup(CommitTypeFor.Feature, 2));
+            changelog.Should().Be(A.Changelog.WithGroup(Feature, 2));
         }
 
         [Fact]
         public void when_a_reverted_reverting_commit_is_part_of_the_changelog_shows_the_originally_reverted_commit()
         {
             var revertingReverting =
-                CommitTypeFor.Feature.CommitWithDescription(3).WithFooter(DefaultRevertToken, _reverting.Hash);
+                Feature.CommitWithDescription(3).WithFooter(DefaultRevertToken, _reverting.Hash);
 
             var changelog = The.ChangelogFrom(revertingReverting, _reverting, _reverted);
 
-            changelog.Should().Be(A.Changelog.WithGroup(CommitTypeFor.Feature, 1));
+            changelog.Should().Be(A.Changelog.WithGroup(Feature, 1));
         }
 
         [Fact]
         public void when_multiple_reverting_commits_target_a_single_commit_that_is_part_of_the_changelog_shows_none_of_these_commits()
         {
-            var reverting2 = CommitTypeFor.Feature.CommitWithDescription(3).WithFooter(DefaultRevertToken, _reverted.Hash);
+            var reverting2 = Feature.CommitWithDescription(3).WithFooter(DefaultRevertToken, _reverted.Hash);
 
             var changelog = The.ChangelogFrom(reverting2, _reverting, _reverted);
 
@@ -64,8 +65,21 @@ public partial class A_changelog_from_changelog_relevant_conventional_commits
         [CaseVariantData(DefaultRevertToken)]
         public void recognizes_reverting_commits_by_the(string footer)
         {
-            var reverting = CommitTypeFor.Feature.CommitWithDescription(2).WithFooter(footer, _reverted.Hash);
+            var reverting = Feature.CommitWithDescription(2).WithFooter(footer, _reverted.Hash);
             var changelog = The.ChangelogFrom(reverting, _reverted);
+
+            changelog.Should().Be(A.Changelog.Empty);
+        }
+
+        [Theory]
+        [CaseVariantData(DefaultRevertToken)]
+        public void recognizes_reverting_multiple_commits(string footer)
+        {
+            var reverted2 = Feature.CommitWithDescription(2);
+            var reverting = Feature.CommitWithDescription(3)
+                .WithFooter(footer, _reverted.Hash)
+                .WithFooter(footer, reverted2.Hash);
+            var changelog = The.ChangelogFrom(reverting, reverted2, _reverted);
 
             changelog.Should().Be(A.Changelog.Empty);
         }
