4150901fd7e199f22abd57a35f101ca2649d3d96
Crown0815 <felix.kroener@gmx.de>
2026-05-13 16:36:13 +0200

    fix: Remove unnecessary escaping in GitHub output generation

--- src/ConventionalChangelog/BuildSystems/GitHub.cs
+++ src/ConventionalChangelog/BuildSystems/GitHub.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace ConventionalChangelog.BuildSystems;
 
 public static class GitHub
@@ -18,7 +16,7 @@ public static class GitHub
     // see https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-an-output-parameter
     public static string GenerateContent(string name, string value)
     {
-        return $"{name}<<EOF{Environment.NewLine}{Escaped(value)}{Environment.NewLine}EOF";
+        return $"{name}<<EOF{Environment.NewLine}{value}{Environment.NewLine}EOF";
     }
 
     public static bool IsCurrentCi()
@@ -26,20 +24,4 @@ public static class GitHub
         return Environment.GetEnvironmentVariable(EnvironmentVariable) is not null
             && Environment.GetEnvironmentVariable(EnvironmentOutputVariable) is not null;
     }
-
-    private static string Escaped(string raw) => raw
-        .ReplaceUniCode();
-
-    private static string ReplaceUniCode(this string raw) =>
-        raw.Aggregate(new StringBuilder(), Append).ToString();
-
-    private static StringBuilder Append(StringBuilder b, char character) =>
-        b.Append(Escaped(character));
-
-    private static object Escaped(char c) => char.IsAscii(c)
-        ? c
-        : Escaped((int)c);
-
-    private static string Escaped(int c) => "\\u" + c.ToString("x4");
-
 }
